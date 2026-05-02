4234ddb1673dddf413c543b438b64a8a0a5e3ab9
Valentin Caulier <projects@vcaulier.com>
2026-05-02 11:02:01 +0200

    feat(app): Replacing Main App component placeholder to our rates & cot reports components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CotChartComponent } from './features/cot-chart/cot-chart.component';
+import { InterestRatesComponent } from './features/interest-rates/interest-rates.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CotChartComponent, InterestRatesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
